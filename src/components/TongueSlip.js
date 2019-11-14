d8c1fdb4a0506681a483bd4c382ca611dede6bb8
Choiyounseo <dbstj72@kaist.ac.kr>
2019-11-14 22:31:49 +0900

    feat(tongueslip): Add VideoRecord component to tongue page

--- src/components/TongueSlip.js
+++ src/components/TongueSlip.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
+import styled from 'styled-components';
+
+import VideoRecord from './VideoRecord';
+// import PoseProgress from './PoseProgress';
+
+import PageTemplate from './PageTemplate';
+
+const Wrapper = styled.div`
+// CSS 여기에 Sass 쓰면됨
+  scroll-snap-align: start;
+	width: 100vw;
+	padding: 70px 0 200px 0;
+	min-height: 100vh;	
+	height: auto;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	position: relative;
+`
 
 const TongueSlip = () => {
-	return (
-		<div>
+  const [src, setSrc] = useState ('')
 
-		</div>
+  const processVideo = (id, video) => {
+		setSrc(window.URL.createObjectURL(video));
+		console.log('videoObject', window.URL.createObjectURL(video))
+	}
+
+	return (
+		<PageTemplate>
+      <VideoRecord id="tongSlip" processVideo={processVideo} />
+      <Wrapper>
+        <button>Upload</button>
+      </Wrapper>
+		</PageTemplate>
 	)
 }
 

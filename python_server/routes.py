b65a30b60a6252333fb56f710d381b3654a9eed7
Choiyounseo <dbstj72@kaist.ac.kr>
2019-11-27 04:29:54 +0900

    feat(speechToText): Add python version speechToText
    Cut, Merge complete

--- python_server/routes.py
+++ python_server/routes.py
@@ -10,6 +10,15 @@ from config import (
   HTTP,
   ADDR,
 )
+from speechToText import (
+  speech_to_text,
+  find_words
+)
+from video_utils import (
+  getAudioLength,
+  getVideoLength,
+  mergeVideos
+)
 
 app = Flask(__name__)
 
@@ -24,6 +33,40 @@ class Temp(Resource):
     def post(self):
       return
     def get(self):
+      # get audio file from frontend wav!
+      # temp
+      video_path = os.path.join(
+        os.path.dirname(__file__),
+        'resources',
+        'test_video.mp4')
+      audio_path = os.path.join(
+        os.path.dirname(__file__),
+        'resources',
+        'audio.raw')
+
+      # duration = getAudioLength(audio_path)
+      # print("duration: ", duration)
+
+      words_list = speech_to_text(audio_path)
+      cutting_list = find_words(words_list)
+
+      # temp cutting_list
+      cutting_list = []
+      cutting_list.append({
+        'cut_start': 3.2,
+        'cut_end': 7,
+      })
+      cutting_list.append({
+        'cut_start': 8,
+        'cut_end': 10,
+      })
+      
+      merge_video_path = os.path.join(
+        os.path.dirname(__file__),
+        'resources',
+        'merge_test_video.mp4')
+      mergeVideos(video_path, merge_video_path, cutting_list)
+
       return 'hello...?'
 
 api.add_resource(Temp, "/temp")
--- python_server/video_utils.py
+++ python_server/video_utils.py
@@ -1,13 +1,39 @@
-from moviepy.editor import VideoFileClip, concatenate_videoclips
+from moviepy.editor import VideoFileClip, concatenate_videoclips, AudioFileClip
 
 def videoToAudio(video_path, audio_path):
     video = VideoFileClip(video_path)
     audio = video.audio
     audio.write_audiofile(audio_path)
 
+def getAudioLength(audio_path):
+    audio = AudioFileClip(audio_path)
+    return audio.duration
+
+def getVideoLength(video_path):
+    video = VideoFileClip(video_path)
+    return video.duration
+
 # to cut video, use subclip(). e.g., clip2 = VideoFileClip(video2).subclip(4)
-def mergeVideos(video1, video2, output_path):
-    clip1 = VideoFileClip(video1)
-    clip2 = VideoFileClip(video2)
-    final_clip = concatenate_videoclips([clip1,clip2])
+def mergeVideos(video_path, output_path, cutting_list):
+    video = VideoFileClip(video_path)
+    duration = video.duration
+
+    cut_start = 0
+    cut_end = 0
+    clip_videos = []
+    for i in range(len(cutting_list)+1):
+        if (i == 0):
+          cut_start = 0
+          cut_end = cutting_list[i]['cut_start']
+        elif (i == len(cutting_list)):
+          cut_start = cutting_list[i-1]['cut_end']
+          cut_end = duration
+        else:
+          cut_start = cutting_list[i-1]['cut_end']
+          cut_end = cutting_list[i]['cut_start']
+
+        clip = video.subclip(cut_start, cut_end)
+        clip_videos.append(clip)
+    
+    final_clip = concatenate_videoclips(clip_videos)
     final_clip.write_videofile(output_path, temp_audiofile='temp-audio.m4a', remove_temp=True, codec="libx264", audio_codec="aac")

4ddc0544819553e6886fb086fd5b220aedd33bb4
Choiyounseo <dbstj72@kaist.ac.kr>
2019-11-27 14:46:16 +0900

    feat(speechToText): Complete video merging, adding subtitles
    TODO: need to implement converting video to audio file with right format

--- python_server/routes.py
+++ python_server/routes.py
@@ -17,7 +17,9 @@ from speechToText import (
 from video_utils import (
   getAudioLength,
   getVideoLength,
-  mergeVideos
+  mergeVideos,
+  newWordList,
+  addSubtitles,
 )
 
 app = Flask(__name__)
@@ -54,18 +56,106 @@ class Temp(Resource):
       cutting_list = []
       cutting_list.append({
         'cut_start': 3.2,
-        'cut_end': 7,
+        'cut_end': 7.0,
       })
       cutting_list.append({
-        'cut_start': 8,
-        'cut_end': 10,
+        'cut_start': 8.2,
+        'cut_end': 10.0,
       })
+
+      # temp words_list
+      words_list = [
+        {
+          'value': 'rockbuster',
+          'start_secs': 2.3,
+          'end_secs': 3.2,
+          'speaker_tag': 2
+        },
+        { 'value': 'the', 'start_secs': 3.2, 'end_secs': 4.2, 'speaker_tag': 2 },
+        { 'value': 'stock', 'start_secs': 4.2, 'end_secs': 4.7, 'speaker_tag': 2 },
+        { 'value': 'that', 'start_secs': 4.7, 'end_secs': 4.9, 'speaker_tag': 2 },
+        { 'value': 'the', 'start_secs': 4.9, 'end_secs': 5.0, 'speaker_tag': 2 },
+        { 'value': 'judge', 'start_secs': 5.0, 'end_secs': 5.4, 'speaker_tag': 2 },
+        { 'value': 'that', 'start_secs': 5.4, 'end_secs': 6.7, 'speaker_tag': 2 },
+        { 'value': 'will', 'start_secs': 6.7, 'end_secs': 7.0, 'speaker_tag': 2 },
+        { 'value': 'be', 'start_secs': 7.0, 'end_secs': 7.1, 'speaker_tag': 2 },
+        {
+          'value': 'obstacles',
+          'start_secs': 7.1,
+          'end_secs': 7.4,
+          'speaker_tag': 2
+        },
+        { 'value': 'but', 'start_secs': 7.4, 'end_secs': 8.2, 'speaker_tag': 2 },
+        { 'value': "it's", 'start_secs': 8.2, 'end_secs': 8.4, 'speaker_tag': 2 },
+        { 'value': 'all', 'start_secs': 8.4, 'end_secs': 8.6, 'speaker_tag': 2 },
+        { 'value': 'about', 'start_secs': 8.6, 'end_secs': 8.7, 'speaker_tag': 2 },
+        { 'value': 'finding', 'start_secs': 8.7, 'end_secs': 9.1, 'speaker_tag': 2 },
+        { 'value': 'a', 'start_secs': 9.1, 'end_secs': 9.5, 'speaker_tag': 2 },
+        { 'value': 'way', 'start_secs': 9.5, 'end_secs': 9.7, 'speaker_tag': 2 },
+        {
+          'value': 'through',
+          'start_secs': 9.7,
+          'end_secs': 10.0,
+          'speaker_tag': 2
+        },
+        {
+          'value': 'bushing',
+          'start_secs': 10.0,
+          'end_secs': 11.5,
+          'speaker_tag': 2
+        },
+        { 'value': 'on', 'start_secs': 11.5, 'end_secs': 11.6, 'speaker_tag': 2 },
+        {
+          'value': 'upwards',
+          'start_secs': 11.6,
+          'end_secs': 13.1,
+          'speaker_tag': 2
+        },
+        { 'value': 'until', 'start_secs': 13.9, 'end_secs': 14.5, 'speaker_tag': 2 },
+        {
+          'value': 'finally',
+          'start_secs': 14.5,
+          'end_secs': 14.9,
+          'speaker_tag': 2
+        },
+        {
+          'value': 'reaching',
+          'start_secs': 14.9,
+          'end_secs': 15.7,
+          'speaker_tag': 2
+        },
+        { 'value': 'the', 'start_secs': 15.7, 'end_secs': 15.9, 'speaker_tag': 2 },
+        { 'value': 'talk', 'start_secs': 15.9, 'end_secs': 16.4, 'speaker_tag': 2 },
+        {
+          'value': 'Buxton',
+          'start_secs': 16.4,
+          'end_secs': 17.9,
+          'speaker_tag': 2
+        },
+        { 'value': 'his', 'start_secs': 17.9, 'end_secs': 18.7, 'speaker_tag': 2 },
+        { 'value': 'to', 'start_secs': 18.7, 'end_secs': 18.8, 'speaker_tag': 2 },
+        { 'value': 'the', 'start_secs': 18.8, 'end_secs': 18.9, 'speaker_tag': 2 },
+        {
+          'value': 'up-and-coming',
+          'start_secs': 18.9,
+          'end_secs': 19.6,
+          'speaker_tag': 2
+        }
+      ]
       
       merge_video_path = os.path.join(
         os.path.dirname(__file__),
         'resources',
         'merge_test_video.mp4')
+      subtitle_video_path = os.path.join(
+        os.path.dirname(__file__),
+        'resources',
+        'subtitle_test_video.mp4')
+      
       mergeVideos(video_path, merge_video_path, cutting_list)
+      # mergeVideo의 text time이 필요...
+      new_words_list = newWordList(words_list, cutting_list)
+      addSubtitles(merge_video_path, subtitle_video_path, new_words_list)
 
       return 'hello...?'
 
--- python_server/video_utils.py
+++ python_server/video_utils.py
@@ -1,4 +1,14 @@
-from moviepy.editor import VideoFileClip, concatenate_videoclips, AudioFileClip
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+from moviepy.editor import (
+  VideoFileClip,
+  concatenate_videoclips,
+  AudioFileClip,
+  TextClip,
+  CompositeVideoClip,
+)
+from moviepy.video.tools.subtitles import SubtitlesClip
+import os.path as op
 
 def videoToAudio(video_path, audio_path):
     video = VideoFileClip(video_path)
@@ -37,3 +47,69 @@ def mergeVideos(video_path, output_path, cutting_list):
     
     final_clip = concatenate_videoclips(clip_videos)
     final_clip.write_videofile(output_path, temp_audiofile='temp-audio.m4a', remove_temp=True, codec="libx264", audio_codec="aac")
+
+
+def newWordList(words_list, cutting_list):
+    time_interval = 0
+    new_words_list = []
+    j = 0
+
+    for i in range(len(words_list)):
+        word =words_list[i]
+        start_secs = word['start_secs']
+        end_secs = word['end_secs']
+        cut_start = cutting_list[j]['cut_start']
+        cut_end = cutting_list[j]['cut_end']
+
+        if (start_secs < cut_start) or (end_secs > cut_end):
+            # word not cutted!
+            new_words_list.append({
+                'value': word['value'],
+                'start_secs': start_secs - time_interval,
+                'end_secs': end_secs - time_interval,
+                'speaker_tag': word['speaker_tag'],
+            })
+        else:
+            # cutted words
+            if (i != len(words_list)-1):
+                next_word = words_list[i+1]
+                next_word_end_secs = next_word['end_secs']
+                if (next_word_end_secs > cut_end):
+                    # word is last word cutted by j-th cutting_element
+                    if (j < len(cutting_list)-1):
+                        j += 1
+                    time_interval += cut_end - cut_start
+    return new_words_list
+
+
+def annotate(clip, txt, txt_color='white', fontsize=30, font='Arial'):
+    """ Writes a text at the bottom of the clip. """
+    txtclip = TextClip(txt, fontsize=fontsize, font=font, color=txt_color)
+    cvc = CompositeVideoClip([clip, txtclip.set_pos(('center', 'bottom'))])
+    return cvc.set_duration(clip.duration)
+
+def addSubtitles(video_path, output_path, words_list):
+    # video = merged new video file
+    video = VideoFileClip(video_path)
+    duration = video.duration
+
+    subs = []
+    sub_strings = ''
+
+    start_secs = 0
+    for i in range(len(words_list)):
+        word = words_list[i]
+        sub_strings += word['value'] + ' '
+
+        if (len(sub_strings) > 20):
+            subs.append(((start_secs, word['end_secs']), sub_strings))
+            sub_strings = ''
+            if (i != len(words_list)-1):
+                next_word = words_list[i+1]
+                start_secs = next_word['start_secs']
+        elif (i == len(words_list)-1):
+            subs.append(((start_secs, word['end_secs']), sub_strings))
+
+    annotated_clips = [annotate(video.subclip(from_t, to_t), txt) for (from_t, to_t), txt in subs]
+    final_clip = concatenate_videoclips(annotated_clips)
+    final_clip.write_videofile(output_path,  fps=video.fps, temp_audiofile="temp-audio.m4a", remove_temp=True, codec="libx264", audio_codec="aac")

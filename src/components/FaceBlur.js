22bf54b86b3ab08b3ab805422c97c17567214b92
Yuna Seol <yunaseol@kaist.ac.kr>
2019-11-29 03:13:04 +0900

    feat(file Upload): Upload file on frontend

--- src/components/FaceBlur.js
+++ src/components/FaceBlur.js
@@ -1,12 +1,11 @@
-import React, { useRef } from 'react';
+import React, { useState, useCallback } from 'react';
 import styled from 'styled-components';
 
-import VideoRecord from './VideoRecord';
-import PoseProgress from './PoseProgress';
+// import VideoRecord from './VideoRecord';
+// import PoseProgress from './PoseProgress';
 
-import PageTemplate from "./PageTemplate" 
-import * as faceapi from 'face-api.js';
-import sampleVideo from "./face_video.mp4"
+import PageTemplate from "./PageTemplate"
+import Upload from "./Upload"
 
 const Wrapper = styled.div`
 	display: flex;
@@ -24,42 +23,15 @@ const Wrapper = styled.div`
 		height: auto;
 	}
 `
-
-async function faceDetect(inputVideo, canvas) {
-	// await faceapi.loadMtcnnModel('/models')
-	// await faceapi.loadFaceRecognitionModel('/models')
-	await faceapi.loadSsdMobilenetv1Model('/models')
-	console.log('FACE DETECT ')
-	console.log(inputVideo)
-	const displaySize = { width: inputVideo.width, height: inputVideo.height };
-	console.log(displaySize)
-	faceapi.matchDimensions(canvas, displaySize);
-
-	setInterval(async () => {
-		const detections = await faceapi.detectAllFaces(inputVideo)
-		console.log(detections)
-		const resizedDetections = faceapi.resizeResults(detections, displaySize);
-		console.log(resizedDetections)
-		canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-		canvas.getContext('2d').drawImage(inputVideo, 0, 0, displaySize.width, displaySize.height)
-		faceapi.draw.drawDetections(canvas, resizedDetections)
-	}, 100);
-}
-
 const FaceBlur = () => {
-	const inputVideo = useRef(null)
-	const canvas = useRef(null)
-	const onPlay = () => {
-		faceDetect(inputVideo.current, canvas.current)
-	}
-
 	return (
 		<PageTemplate>
 			<Wrapper>
 				<img src="http://localhost:5000/video_feed" />
-				<video id="abc" width="320" height="179" autoPlay controls src={ sampleVideo } ref={inputVideo} onPlay={onPlay}/>
-				<canvas ref={canvas}/>
+				{/* <video id="abc" width="320" height="179" autoPlay controls src={ sampleVideo } ref={inputVideo} onPlay={onPlay}/> */}
+				{/* <canvas ref={canvas}/> */}
 			</Wrapper>
+			<Upload />
 		</PageTemplate>
 	)
 }

c7cd59e18c8be63a795e03eed5946fbb8f29934b
Yuna Seol <yunaseol@kaist.ac.kr>
2019-11-30 15:50:59 +0900

    feature (face blur): download blurred video

--- python_server/blur_utils.py
+++ python_server/blur_utils.py
@@ -3,8 +3,8 @@ import video_utils
 import face_models
 
 def blurAllFaces(video_path, output_path):
-    model = "input/res10_300x300_ssd_iter_140000[1].caffemodel"
-    prototxt = "input/deploy.prototxt[1].txt"
+    model = "input/res10_300x300_ssd_iter_140000.caffemodel"
+    prototxt = "input/deploy.prototxt"
     confidence = 0.5
     net = cv2.dnn.readNetFromCaffe(prototxt, model)
     video_utils.processVideo(video_path, output_path, face_models.faceDetectBlur, net, confidence)
@@ -15,4 +15,4 @@ def blurOtherFaces(video_path, output_path):
     video_utils.processVideo(video_path, output_path, fr.faceRecogBlur)
 
 # blurAllFaces('media/sample1.mov', 'media/output1.mp4')
-# blurOtherFaces('media/sample1.mov', 'media/output1.mp4')
+# blurOtherFaces('uploads/yunagyurac.mov', 'media/output1.mp4')

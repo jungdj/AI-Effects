aeac80ef5ff57931cdcad62fa38e032b04117717
jungdj <jung_dj@kaist.ac.kr>
2019-11-30 22:29:38 +0900

    [Front] feat(dashboard): Add dashboard layout

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,74 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	* {
+		display: flex;
+	}
+	section {
+		border: 0.1px solid lightslategrey;
+	}
+`
+
+const Row1 = styled.section`
+	width: 100%;
+	flex: 7;
+	
+	.panel-1 {
+		flex: 1;
+		flex-direction: column;
+		.panel-1-1 {
+			flex: 2;
+		}
+		.panel-1-2 {
+			flex: 1;
+		}
+	}
+	.panel-2 {
+		flex: 3;
+	}
+	.panel-3 {
+		flex: 1;
+		flex-direction: column;
+		.panel-3-1 {
+			flex: 1;
+		}
+		.panel-3-2 {
+			flex: 1;
+		}
+	}
+`
+
+const Row2 = styled.section`
+	display: flex;
+	width: 100%;
+	flex: 3;
+`
 
 const Dashboard = () => {
 	return (
-		<div>
+		<Wrapper>
+			<Row1>
+				<section className="panel-1">
+					<section className="panel-1-1"></section>
+					<section className="panel-1-2"></section>
+				</section>
+				<section className="panel-2">
 
-		</div>
+				</section>
+				<section className="panel-3">
+					<section className="panel-3-1"></section>
+					<section className="panel-3-2"></section>
+				</section>
+			</Row1>
+			<Row2>
+
+			</Row2>
+		</Wrapper>
 	)
 }
 

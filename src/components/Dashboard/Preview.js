c65379a64b5d296513a964ef98932373f85d6d18
jungdj <jung_dj@kaist.ac.kr>
2019-12-01 23:28:42 +0900

    [Front] fix(videoedit): Fix video edit ui, add download

--- src/components/Dashboard/Preview.js
+++ src/components/Dashboard/Preview.js
@@ -2,19 +2,22 @@ import styled from "styled-components"
 import { Player } from "video-react"
 import React from "react"
 
+import download from '../../static/icons/cloud_download-24px.svg'
+
 const PrevWrapper = styled.div`
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
 	width: 100%;
-	height: 100%;
+	height: 80%;
 `
 
 const Preview = ({ previewUrl }) => {
 	return (
 		<PrevWrapper onClick={e => e.stopPropagation()}>
-			<Player playsInline src={previewUrl} />
+			<Player playsInline src={previewUrl} fluid={false} />
+			<a href={previewUrl}><img src={download} /></a>
 		</PrevWrapper>
 	)
 }
